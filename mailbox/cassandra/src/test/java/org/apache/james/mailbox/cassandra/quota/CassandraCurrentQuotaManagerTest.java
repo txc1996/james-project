550ca5160a16628b8aa5372f86dc37431cf1d7a3
Benoit Tellier <btellier@linagora.com>
2016-12-29 10:16:32 +0700

    PVN-5 Factorise test for Current quota Manager

--- mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/quota/CassandraCurrentQuotaManagerTest.java
+++ mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/quota/CassandraCurrentQuotaManagerTest.java
@@ -19,98 +19,24 @@
 
 package org.apache.james.mailbox.cassandra.quota;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.apache.james.backends.cassandra.CassandraCluster;
 import org.apache.james.mailbox.cassandra.modules.CassandraQuotaModule;
-import org.apache.james.mailbox.model.QuotaRoot;
-import org.apache.james.mailbox.store.quota.QuotaRootImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.apache.james.mailbox.store.quota.CurrentQuotaManagerTest;
+import org.apache.james.mailbox.store.quota.StoreCurrentQuotaManager;
 
-public class CassandraCurrentQuotaManagerTest {
+public class CassandraCurrentQuotaManagerTest extends CurrentQuotaManagerTest {
 
-    private static final QuotaRoot QUOTA_ROOT = QuotaRootImpl.quotaRoot("value");
+    private static final CassandraCluster cassandra = CassandraCluster.create(new CassandraQuotaModule());
 
-    private CassandraCluster cassandra;
-    private CassandraCurrentQuotaManager currentQuotaManager;
-
-    @Before
-    public void setUp() {
-        cassandra = CassandraCluster.create(new CassandraQuotaModule());
+    @Override
+    protected StoreCurrentQuotaManager createCurrentQuotaManager() throws Exception {
         cassandra.ensureAllTables();
-        currentQuotaManager = new CassandraCurrentQuotaManager(cassandra.getConf());
+        return new CassandraCurrentQuotaManager(cassandra.getConf());
     }
 
-    @After
-    public void cleanUp() {
+    @Override
+    protected void clear() throws Exception {
         cassandra.clearAllTables();
-    }
-
-    @Test
-    public void getCurrentStorageShouldReturnZeroByDefault() throws Exception {
-        assertThat(currentQuotaManager.getCurrentStorage(QUOTA_ROOT)).isEqualTo(0);
-    }
-
-    @Test
-    public void getCurrentMessageCountShouldReturnZeroByDefault() throws Exception {
-        assertThat(currentQuotaManager.getCurrentMessageCount(QUOTA_ROOT)).isEqualTo(0);
-    }
-
-    @Test
-    public void increaseShouldWork() throws Exception {
-        currentQuotaManager.increase(QUOTA_ROOT, 2, 2000);
-        assertThat(currentQuotaManager.getCurrentStorage(QUOTA_ROOT)).isEqualTo(2000);
-        assertThat(currentQuotaManager.getCurrentMessageCount(QUOTA_ROOT)).isEqualTo(2);
-    }
-
-    @Test
-    public void decreaseShouldWork() throws Exception {
-        currentQuotaManager.increase(QUOTA_ROOT, 2, 2000);
-        currentQuotaManager.decrease(QUOTA_ROOT, 1, 1000);
-        assertThat(currentQuotaManager.getCurrentStorage(QUOTA_ROOT)).isEqualTo(1000);
-        assertThat(currentQuotaManager.getCurrentMessageCount(QUOTA_ROOT)).isEqualTo(1);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void increaseShouldThrowOnZeroCount() throws Exception {
-        currentQuotaManager.increase(QUOTA_ROOT, 0, 5);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void increaseShouldThrowOnNegativeCount() throws Exception {
-        currentQuotaManager.increase(QUOTA_ROOT, -1, 5);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void increaseShouldThrowOnZeroSize() throws Exception {
-        currentQuotaManager.increase(QUOTA_ROOT, 5, 0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void increaseShouldThrowOnNegativeSize() throws Exception {
-        currentQuotaManager.increase(QUOTA_ROOT, 5, -1);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void decreaseShouldThrowOnZeroCount() throws Exception {
-        currentQuotaManager.decrease(QUOTA_ROOT, 0, 5);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void decreaseShouldThrowOnNegativeCount() throws Exception {
-        currentQuotaManager.decrease(QUOTA_ROOT, -1, 5);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void decreaseShouldThrowOnZeroSize() throws Exception {
-        currentQuotaManager.decrease(QUOTA_ROOT, 5, 0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void decreaseShouldThrowOnNegativeSize() throws Exception {
-        currentQuotaManager.decrease(QUOTA_ROOT, 5, -1);
     }
 
 }
