c1d626e36d3b6dab6aca88f8a29e488d7a673f76
benwa <btellier@linagora.com>
2017-03-21 09:23:58 +0700

    PNV-18 Add test skeleton

---
+++ mailet/standard/src/test/java/org/apache/james/transport/matchers/LimitRecipientsTest.java
@@ -0,0 +1,123 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.transport.matchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class LimitRecipientsTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private LimitRecipients testee;
+
+    @Before
+    public void setUp() {
+        testee = new LimitRecipients();
+    }
+
+    @Test
+    public void initShouldThrowOnAbsentCondition() {
+        /*
+        What happens if there is no condition?
+
+        We should throw an exception
+         */
+    }
+
+    @Test
+    public void initShouldThrowOnInvalidCondition() {
+        /*
+        What happens when condition is not a number? We should throw.
+         */
+    }
+
+    @Test
+    public void initShouldThrowOnEmptyCondition() {
+        /*
+        What happens when condition is empty? We should throw.
+         */
+    }
+
+    @Test
+    public void initShouldThrowOnZeroCondition() {
+        /*
+        What happens when condition is zero? We should throw.
+         */
+    }
+
+    @Test
+    public void initShouldThrowOnNegativeCondition() {
+        /*
+        What happens when condition is negative? We should throw.
+         */
+    }
+
+    @Test
+    public void matchShouldReturnNoRecipientWhenMailHaveNoRecipient() {
+        /*
+        Start the matcher with condition = 3
+
+        What happens with a mail with 0 recipients?
+
+        We should return empty list
+         */
+    }
+
+    @Test
+    public void matchShouldNotModifyMailsUnderLimit() {
+        /*
+        Start the matcher with condition = 3
+
+        What happens with a mail with 1 recipients?
+
+        We should return a list of this recipient
+         */
+    }
+
+
+    @Test
+    public void matchShouldNotModifyMailsAtLimit() {
+        /*
+        Start the matcher with condition = 3
+
+        What happens with a mail with 3 recipients?
+
+        We should return a list with these 3 recipients
+         */
+    }
+    
+    @Test
+    public void matchShouldLimitRecipientCountWhenMailsOverLimit() {
+        /*
+        Start the matcher with condition = 3
+
+        What happens with a mail with 4 recipients?
+
+        We should return a list with the first 3 recipients
+         */
+    }
+
+}
